0ca9773a11cf8f6a3a90dbc2cd3059ba41243b19
jp <jp@creaker.cn>
2016-02-15 16:07:45 +0800

    初始版本，测试。

---
+++ booksapp/booksapp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  booksapp
+//
+//  Created by 姬鹏 on 16/2/15.
+//  Copyright © 2016年 kunlun publisher. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
